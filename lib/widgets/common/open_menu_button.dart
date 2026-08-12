2a707371eaa96e1602f48722d4a99036a51fb3bb
saravana pmv <saravanapmvofficial@gmail.com>
2026-08-12 15:36:07 +0530

    Remove backside gradient blur box shadow from OpenMenuButton

--- lib/widgets/common/open_menu_button.dart
+++ lib/widgets/common/open_menu_button.dart
@@ -8,7 +8,7 @@ class OpenMenuButton extends StatelessWidget {
   const OpenMenuButton({
     super.key,
     required this.onTap,
-    this.height = 68,
+    this.height = 48,
   });
 
   @override
@@ -17,66 +17,51 @@ class OpenMenuButton extends StatelessWidget {
       onTap: onTap,
       child: Container(
         height: height,
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: const Color(0xFF1D4ED8).withValues(alpha: 0.4),
-              blurRadius: 16,
-              offset: const Offset(4, 4),
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.horizontal(right: Radius.circular(height / 2)),
-          child: Image.asset(
-            'assets/menu_icon_tab.png',
-            height: height,
-            fit: BoxFit.contain,
-            errorBuilder: (context, error, stackTrace) {
-              return Image.asset(
-                'assets/sidebar_icon.png',
-                height: height,
-                fit: BoxFit.contain,
-                errorBuilder: (context, error, stackTrace) {
-                  return Image.asset(
-                    'assets/open_menu.png',
-                    height: height,
-                    fit: BoxFit.contain,
-                    errorBuilder: (context, error, stackTrace) {
-                      return Container(
-                        height: height,
-                        padding: const EdgeInsets.fromLTRB(14, 0, 20, 0),
-                        decoration: BoxDecoration(
-                          gradient: const LinearGradient(
-                            colors: [Color(0xFF1D4ED8), Color(0xFF2563EB)],
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
+        color: Colors.transparent,
+        child: Image.asset(
+          'assets/menu_icon_tab.png',
+          height: height,
+          fit: BoxFit.contain,
+          errorBuilder: (context, error, stackTrace) {
+            return Image.asset(
+              'assets/sidebar_icon.png',
+              height: height,
+              fit: BoxFit.contain,
+              errorBuilder: (context, error, stackTrace) {
+                return Image.asset(
+                  'assets/open_menu.png',
+                  height: height,
+                  fit: BoxFit.contain,
+                  errorBuilder: (context, error, stackTrace) {
+                    return Container(
+                      height: height,
+                      padding: const EdgeInsets.fromLTRB(10, 0, 16, 0),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF2563EB),
+                        borderRadius: BorderRadius.horizontal(right: Radius.circular(height / 2)),
+                      ),
+                      child: const Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.chevron_right_rounded,
+                            color: Colors.white,
+                            size: 22,
                           ),
-                          borderRadius: BorderRadius.horizontal(right: Radius.circular(height / 2)),
-                        ),
-                        child: const Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(
-                              Icons.chevron_right_rounded,
-                              color: Colors.white,
-                              size: 28,
-                            ),
-                            SizedBox(width: 4),
-                            Icon(
-                              Icons.menu_rounded,
-                              color: Colors.white,
-                              size: 26,
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  );
-                },
-              );
-            },
-          ),
+                          SizedBox(width: 4),
+                          Icon(
+                            Icons.menu_rounded,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          },
         ),
       ),
     );
